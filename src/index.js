43bc129c305997428f6484b2935ff07db4a43951
Andrey35211 <serand3521@yandex.ru>
2022-10-30 13:57:18 +0700

    42 redux

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import store from "./redux/state";
+import store from "./redux/redux-store";
 import './index.css';
 import App from './App';
 
@@ -17,7 +17,10 @@ let rerenderEntireTree = (state) => {
 
 rerenderEntireTree(store.getState());
 
-store.subscribe(rerenderEntireTree);
+store.subscribe(()=> {
+    let state = store.getState();
+    rerenderEntireTree();
+});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- src/redux/dialogs-reducer.js
+++ src/redux/dialogs-reducer.js
@@ -1,7 +1,23 @@
 const UPDATE_NEW_MESSAGE_BODY = 'UPDATE_NEW_MESSAGE_BODY';
 const SEND_MESSAGE = "SEND_MESSAGE"
 
-const dialogsReducer = (state, action) => {
+let initialState = {
+    dialogs: [
+        {id: 1, name: 'Vlad'},
+        {id: 2, name: 'Andrey'},
+        {id: 3, name: 'Veronika'},
+        {id: 4, name: 'Katya'},
+        {id: 5, name: 'Sasha'},
+    ],
+    messages: [
+        {id: 1, message: 'Hi'},
+        {id: 2, message: 'How are you?'},
+        {id: 3, message: 'Yo'},
+    ],
+    newMessageBody: ""
+};
+
+const dialogsReducer = (state = initialState, action) => {
 
     switch (action.type) {
         case UPDATE_NEW_MESSAGE_BODY:
